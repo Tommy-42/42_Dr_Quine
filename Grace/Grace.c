6bfd03f5cc56ed2187d1a15960a2845d54f1dfcd
Tommy PAGEARD <tommy.pageard@student.42.fr>
2016-03-25 14:32:15 +0100

    print itself into a child file

--- Grace/Grace.c
+++ Grace/Grace.c
@@ -1,3 +1,5 @@
 #include <stdio.h>
-#define FT(x) int main() { FILE *fp = fopen("Grace_kid.c","w"); char *c="#include <stdio.h>%c#define FT(x) int main() { FILE *fp = fopen(%cGrace_kid.c%c,%cw%c); char *c=%c%s%c;fprintf(fp,c,10,34,34,34,34,34,c,34,10,10); fclose(fp); }%cFT()%c";fprintf(fp,c,10,34,34,34,34,34,c,34,10,10); fclose(fp); }
+#define A(y) void F_A() {}
+#define B(z) void F_B() {}
+#define FT(x) int main() { FILE *fp = fopen("Grace_kid.c","w"); char *c="#include <stdio.h>%c#define A(y) void F_A() {}%c#define B(z) void F_B() {}%c#define FT(x) int main() { FILE *fp = fopen(%cGrace_kid.c%c,%cw%c); char *c=%c%s%c;fprintf(fp,c,10,10,10,34,34,34,34,34,c,34,10,10); fclose(fp); }%cFT()%c";fprintf(fp,c,10,10,10,34,34,34,34,34,c,34,10,10); fclose(fp); }
 FT()
